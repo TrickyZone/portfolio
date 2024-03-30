58e3be20083c11c1760048e7438a453c296e00e6
muzakkirsaifi123 <mohd.saifi@nashtechglobal.com>
2024-03-31 01:59:14 +0530

    Updates

---
+++ precache-manifest.5bd815d237b5a4af77c1aebd33c92c2c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548221c8125f6f6b50300a81b4ac2096",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d32711ed29332369cd9f",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "93eb10e51f539886455d",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "bd72adb4765a9e0eca8e",
+    "url": "/portfolio/static/css/main.fd348e5e.chunk.css"
+  },
+  {
+    "revision": "75a349833cc129bea189",
+    "url": "/portfolio/static/js/2.0b0f4f6d.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/portfolio/static/js/2.0b0f4f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32711ed29332369cd9f",
+    "url": "/portfolio/static/js/3.dba2273e.chunk.js"
+  },
+  {
+    "revision": "93eb10e51f539886455d",
+    "url": "/portfolio/static/js/4.2d2210e0.chunk.js"
+  },
+  {
+    "revision": "bd72adb4765a9e0eca8e",
+    "url": "/portfolio/static/js/main.72701455.chunk.js"
+  },
+  {
+    "revision": "9c71bb8313bb91350944",
+    "url": "/portfolio/static/js/runtime-main.6ca60dd2.js"
+  },
+  {
+    "revision": "2fcc1a93f407e62e4c2e60ab8e722f4f",
+    "url": "/portfolio/static/media/3TL.2fcc1a93.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "9a6fc1f361ca7ebcabb79c7f5c2d3589",
+    "url": "/portfolio/static/media/DCT.9a6fc1f3.webp"
+  },
+  {
+    "revision": "a639846323aa90426abbd36d9c489932",
+    "url": "/portfolio/static/media/GCP_associate_certficate.a6398463.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "834905b4578e7a223cce57b5cd3e1018",
+    "url": "/portfolio/static/media/azure_fun.834905b4.webp"
+  },
+  {
+    "revision": "11c1bbe87e782ced68bf9f063f3fbb43",
+    "url": "/portfolio/static/media/badge-devops-expert.11c1bbe8.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c33e261e126905f0d1c32f163f9b6e3c",
+    "url": "/portfolio/static/media/microsoft-certified-associate.c33e261e.webp"
+  },
+  {
+    "revision": "1721ca65b9314a4f36d2ff6cc21aafd8",
+    "url": "/portfolio/static/media/skill.1721ca65.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
